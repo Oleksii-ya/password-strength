6b8fe622dc379cc7c025ebfd939f239fe328c9bc
Oleksii-ya <prog8dev@gmail.com>
2023-02-02 22:56:22 +0200

    added show + check Latin

--- src/components/Password.js
+++ src/components/Password.js
@@ -11,9 +11,16 @@ const Password = () => {
       medium: "",
       strong: ""
     })
+  const [typeButton, setTypeButton] = useState('password')
 
   const inputHandler = (e) => {
     const curWord = e.target.value
+
+    if (curWord[curWord.length - 1].match(/[а-я]/i)) {
+      alert('Please use Latin')
+      return
+    }
+
 
     const isQuantity = curWord.length > 0 && curWord.length < 8
     const isNumber = !!curWord.match(/[0-9]/)
@@ -39,15 +46,19 @@ const Password = () => {
     <>
       <input
         className="pass-word"
-        type="password"
+        type={typeButton}
         placeholder="Enter Password"
         value={password}
         onChange={inputHandler}
       />
+      <button
+        className="show-password"
+        onClick={() => { setTypeButton(typeButton === 'password' ? 'text' : 'password') }}
+        type="text">Show</button>
       <div className="strength-wrapper">
         <span className={`strength ${quantity} ${easy} ${medium} ${strong}`}></span>
-        <span className={`strength ${quantity}  ${medium} ${strong}`}></span>
-        <span className={`strength ${quantity}  ${strong}`}></span>
+        <span className={`strength ${quantity} ${medium} ${strong}`}></span>
+        <span className={`strength ${quantity} ${strong}`}></span>
       </div>
     </>
   )
